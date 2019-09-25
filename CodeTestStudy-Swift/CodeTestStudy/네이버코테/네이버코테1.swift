debff4a1d8c5196fae3477ac0bae7c8fb3811cb2
namsoo5 <are34@naver.com>
2019-09-25 16:13:50 +0900

    네이버코테 1

--- CodeTestStudy-Swift/CodeTestStudy/네이버코테/네이버코테1.swift
+++ CodeTestStudy-Swift/CodeTestStudy/네이버코테/네이버코테1.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+class 네이버문제1{
+    let topLevel = ["com", "net", "org"]
+    func solution(_ emails: [String]) -> Int {
+        var result = 0
+        for email in emails{
+            if checkEmail(email: email) {
+                result += 1
+            }
+        }
+    
+        print(result)
+        return result
+    }
+    
+    func checkEmail(email: String) -> Bool{
+        
+        let sub = email.split(separator: "@")
+        
+        if sub.count != 2{  // @가 2개이상이거나 @앞이나 뒤가 비어있는경우에러
+            return false
+        }
+        
+        let domain = sub[1].split(separator: ".")
+        
+        if domain.count != 2{ // 도메인부분 . 이 2개이상이거나 없을때에러
+            return false
+        }
+    
+        for top in topLevel{
+            if domain[1] == top {
+                return true
+            }
+        }
+        
+        return false
+    }
+}
