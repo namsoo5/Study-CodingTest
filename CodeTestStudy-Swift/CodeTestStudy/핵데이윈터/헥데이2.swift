5dee1c9992ee1fbb90abb08bbda82438e98aedbf
namsoo5 <are34@naver.com>
2019-10-12 13:50:47 +0900

    헥데이 윈터

--- CodeTestStudy-Swift/CodeTestStudy/핵데이윈터/헥데이2.swift
+++ CodeTestStudy-Swift/CodeTestStudy/핵데이윈터/헥데이2.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+//("<v>AB^CYv^XAZ", ["v>AB^CYv^XA","<v>A","^XAZ","Yv^XA",">AB^"], [50, 18, 20, 30, 25])
+class 헥데이2{
+
+    func solution(_ command:String, _ buttons:[String], _ scores:[Int]) -> Int {
+        
+        let result = check(command: command, btArray: buttons, score: scores)
+        print(result)
+        return result
+    }
+    
+    func check(command: String, btArray: [String], score:[Int]) -> Int{
+        
+        var confirmIndex = [Int]()
+        var index = 0
+        for bt in btArray{
+            if command.contains(bt){
+                confirmIndex.append(index)
+            }
+            index += 1
+        }
+    
+        if confirmIndex.count == 0{
+            return command.count
+        }
+        
+        var max = command.count
+        for i in confirmIndex{
+            var sum = 0
+            let sub = command.components(separatedBy: btArray[i])
+            sum += score[i]
+            for s in sub{
+                sum += check(command: s, btArray: btArray, score: score)
+            }
+            
+            if sum > max {
+                max = sum
+            }
+        }
+       
+        return max
+    }
+}
--- CodeTestStudy-Swift/CodeTestStudy/핵데이윈터/헥데이3.swift
+++ CodeTestStudy-Swift/CodeTestStudy/핵데이윈터/헥데이3.swift
@@ -7,3 +7,100 @@
 //
 
 import Foundation
+
+class 헥데이3{
+    
+    func solution(_ maps:[String]) -> [Int] {
+        
+        var hash = [String:[String]]()
+        
+        for row in maps{
+            print(row)
+            var before = "."
+            for s in row{  //좌우 공유확인
+                let key = String(s)
+                
+                if before == "." || key == before || key == "."{
+                    before = String(s)
+                    continue
+                }
+                
+                 
+                
+                before = String(s)
+            }
+        }
+        
+        print(hash)
+        
+        
+        return []
+    }
+}
+
+func solution2(_ maps:[String]) -> [Int] {
+    
+    var relation = [Character: [Character]]()
+    let maps = maps.map { $0.map { $0 as Character} }
+    
+    for y in maps.indices {
+        for x in maps[0].indices {
+            if maps[y][x] == "." { continue }
+            let adjacents = confirm(maps: maps, x: x, y: y)
+            relation[maps[y][x]] = Set((relation[maps[y][x]] ?? []) + adjacents).map { $0 }
+        }
+    }
+    let paths = relation.reduce(0) { $0 + $1.value.count }
+    let max = relation.reduce(0) { max, new in
+        max < new.value.count ? new.value.count : max
+    }
+    return [paths/2, max]
+}
+
+func confirm (maps: [[Character]], x: Int, y: Int) -> [Character] {
+    var answer: [Character] = []
+    
+    let height = maps.count
+    let width = maps[0].count
+    
+    let contry = maps[y][x]
+    let none: Character = "."
+    
+    typealias coordinate = (x: Int, y: Int)
+    
+    let operation = [(x:-1, y:0),
+                     (x:0, y:-1),
+                     (x:1, y:0),
+                     (x:0, y:1)]
+    
+    let adjacents = operation
+        .map { (x: x+$0.x, y: y+$0.y) }
+        .filter { ($0.x >= 0 && $0.x < width && $0.y >= 0  && $0.y < height) }
+    
+    for adjacent in adjacents {
+        
+        let current = maps[adjacent.y][adjacent.x]
+        switch (current) {
+        case none:
+            ()
+        case contry:
+            ()
+        default:
+            answer.append(current)
+        }
+    }
+    return answer
+}
+
+//print(solution2( ["……….",
+//            "AAACC…..",
+//            ".AAA…..Z",
+//            "..AAAA..C.",
+//            "…BBBBB..",
+//            "….BBB…",
+//            "…ZBBB…",
+//            "ZZZZAAAC..",
+//            "…..CCCC.",
+//            "QQ……C.",
+//            "………."]))
+//           ".B.C.D."])
